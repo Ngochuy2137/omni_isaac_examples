2728faec6835711e8ec804f82502051fcb88cddc
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2024-05-06 18:18:00 +0900

    Add git ignore to ignore __pycache__ folder
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- user_examples/hello_world_extension.py
+++ user_examples/hello_world_extension.py
@@ -1,16 +1,6 @@
-# Copyright (c) 2020-2023, NVIDIA CORPORATION. All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto. Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-#
-
 import os
-
 from omni.isaac.examples.base_sample import BaseSampleExtension
-from omni.isaac.examples.hello_world import HelloWorld
+from omni.isaac.examples.user_examples import HelloWorld
 
 
 class HelloWorldExtension(BaseSampleExtension):
@@ -19,9 +9,9 @@ class HelloWorldExtension(BaseSampleExtension):
         super().start_extension(
             menu_name="",
             submenu_name="",
-            name="Hello World",
-            title="Hello World Example",
-            doc_link="https://docs.omniverse.nvidia.com/isaacsim/latest/core_api_tutorials/tutorial_core_hello_world.html",
+            name="My Awesome Example",
+            title="My Awesome Example",
+            doc_link="https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/tutorial_core_hello_world.html",
             overview="This Example introduces the user on how to do cool stuff with Isaac Sim through scripting in asynchronous mode.",
             file_path=os.path.abspath(__file__),
             sample=HelloWorld(),
